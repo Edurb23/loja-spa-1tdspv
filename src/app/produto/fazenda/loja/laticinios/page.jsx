68b1e46b9fb378caed8a8b3602f4a8b70eb1c05a
Edurb23 <eddurb64@gmail.com>
2023-10-31 18:11:14 -0300

    Criada base de dados json

--- src/app/produto/fazenda/loja/laticinios/page.jsx
+++ src/app/produto/fazenda/loja/laticinios/page.jsx
@@ -1,41 +1,50 @@
-import React from 'react'
 import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function TodosLaticinios() {
-
-        const response = await fetch("http://localhost:3000/api/laticinios");
-        const queijos = await response.json();
-
-
+        let queijos;
+        try{
+            const response = await fetch("http://localhost:3000/api/laticinios/0");
+            queijos = await response.json();
+        }catch(error){
+            console.log(error);
+            redirect("/error");
+        }
   return (
-    <div>
-        <h1>Laticionios</h1>
+    <div className="lat-prod-all">
+
+        <h1>Laticínios</h1>
+
         <table>
             <thead>
-                <tr>
-                    <th>id</th>
+                <tr>    
+                    <th>ID</th>
                     <th>Imagem</th>
                     <th>Nome</th>
-                    <th>Preco</th>
+                    <th>Preço</th>
                     <th>Categoria</th>
-                    <th>Descricao</th>
+                    <th>Descrição</th>
                 </tr>
             </thead>
             <tbody>
-                {queijos.map((queijos) => (
-                    <tr key={queijos.id}>
-                        <td>{queijos.id}</td>
-                        <td><Image src={queijos.imagem} width={100} height={100} alt={queijos.descricao} /></td>
-                        <td>{queijos.nome}</td>
-                        <td>{queijos.preco}</td>
-                        <td>{queijos.categoria}</td>
-                        <td>{queijos.descricao}</td>
+                {queijos.map((queijo) => (
+                    <tr key={queijo.id}>
+                        <td>{queijo.id}</td>
+                        <td>
+                            <Link href={`/produtos/fazenda/loja/laticinios/${queijo.id}`}>
+                            <Image src={queijo.imagem} width={100} height={100} alt={queijo.descricao} /></Link>
+                        </td>
+                        <td>{queijo.nome}</td>
+                        <td>{queijo.preco}</td>
+                        <td>{queijo.categoria}</td>
+                        <td>{queijo.descricao}</td>
                     </tr>
                 ))}
             </tbody>
             <tfoot>
                 <tr>
-                    <td colSpan="6">Ttotal de Laticínios:{queijos.length}</td>
+                    <td colSpan="6">Total de Laticínios: {queijos.length}</td>
                 </tr>
             </tfoot>
         </table>
